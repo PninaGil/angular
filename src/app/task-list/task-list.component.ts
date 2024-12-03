864211ee030f953afcf3e6c60058ca114211739a
PninaGil <p4168633@gmail.com>
2024-12-03 14:36:08 +0200

    delete function

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -7,4 +7,10 @@ import { Task } from '../task.model';
 })
 export class TaskListComponent {
  tasks:Task[] = [{id:123, name:"Wash dishes"},{id:456, name:"hang laundry"},{id:789, name:"cook a meal"}]
+ 
+ deleteTask(task:Task){
+  let index = this.tasks.indexOf(task)
+  this.tasks.splice(index,1)
+ }
+
 }
